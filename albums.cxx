39282506e0a34152652d9a630318e110d784faed
IvanR <yu181219710>
2020-01-10 15:25:22 +0500

    done

--- albums.cxx
+++ albums.cxx
@@ -1,9 +1,9 @@
 #GRAMMAR_ROOT S
 
-Lang -> 'англ' | 'рус';
+Lang -> 'англ' | 'русс' | 'рус';
 Date->AnyWord<kwtype = "даты">;
-Brackets->LBracket (Lang) AnyWord* (Lang) (AnyWord*) RBracket;
-FirstPart->AnyWord* interp(Albums.Name) (Brackets interp(Albums.Translate)) Hyphen Adj;
+Brackets->LBracket (AnyWord*) Lang (Comma) (Hyphen) AnyWord* interp(Albums.Translate) RBracket;
+FirstPart->AnyWord* interp(Albums.Name) (Brackets ) Hyphen Adj;
 FullName->AnyWord<h-reg1>* AnyWord<h-reg1>;
-Auth -> AnyWord * 'альбом'  AnyWord<l-reg>* FullName interp(Albums.Author) ',';
-S->FirstPart Auth  AnyWord*  Date interp(Albums.Date) (AnyWord *)( ',');
+Auth -> AnyWord * 'альбом'  AnyWord<l-reg>* FullName interp(Albums.Author) Comma;
+S->FirstPart Auth  AnyWord*  Date interp(Albums.Date) (AnyWord *)(Comma);
